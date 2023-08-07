0b618b0376bedc2788f363fff182171fbaaf2c72
EmitoStuer <stuer.87@gmail.com>
2023-08-07 19:32:41 -0300

    Nuevo proyecto
    Agregue un nuevo proyecto para que puedan agregar sus nombre porque no pude acceder al app.java de Jose.

---
+++ ProbandoGit/src/probandogit/ProbandoGit.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probandogit;
+
+/**
+ *
+ * @author Emito
+ */
+public class ProbandoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Arme este nuevo Archivo para intentar subir a la carpeta compartida
+        //ya que no pude acceder al archivo App.java creado por Jose.
+        System.out.println("Stuer Emiliano");
+    }
+    
+}
